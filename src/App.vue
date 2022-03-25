fee1d8d81dfb95ebb4e436497da45570fb8bce73
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-25 22:29:13 +0900

    show data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{data}}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Data from "../data/formatted_data.json"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      data: Data
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
