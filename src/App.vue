5b9a63b6db1d9fc40e11140bf6a566a3ec34832b
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-26 08:46:41 +0900

    get info

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    {{merchant}}
+    {{area}}
+    {{category}}
   </div>
 </template>
 
 <script>
 import Merchant from "../data/merchant.json"
+import Area from "../data/area.json"
+import Category from "../data/category.json"
+
 export default {
   data() {
     return {
       merchant: Merchant,
+      area: Area,
+      category: Category
     }
   }
 }
