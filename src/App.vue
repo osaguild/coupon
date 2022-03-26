44f94c7df64e100815e15577b215d23e92cf9726
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-26 09:03:29 +0900

    enable specify area / category from ui

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
-    {{area}}
-    {{category}}
+    <select v-model="selected_area">
+      <option v-for="area in areas" :key="area">
+        {{ area }}
+      </option>
+    </select>
+    <span> {{ selected_area }} </span>
+  </div>
+  <div>
+    <select v-model="selected_category">
+      <option v-for="category in categorys" :key="category">
+        {{ category }}
+      </option>
+      {{
+        selected_category
+      }}
+    </select>
+    <span> {{ selected_category }} </span>
   </div>
 </template>
 
 <script>
-import Merchant from "../data/merchant.json"
-import Area from "../data/area.json"
-import Category from "../data/category.json"
+import Merchant from "../data/merchant.json";
+import Area from "../data/area.json";
+import Category from "../data/category.json";
 
 export default {
   data() {
     return {
-      merchant: Merchant,
-      area: Area,
-      category: Category
-    }
-  }
-}
+      merchants: Merchant,
+      areas: Area,
+      categorys: Category,
+      selected_area: "",
+      selected_category: "",
+    };
+  },
+};
 </script>
