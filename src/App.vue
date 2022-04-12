499de01ca93d926ae2edfcb27676d43fa9d9452b
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-13 08:24:44 +0900

    fix navi

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Navi
-          </v-list-item-title>
+          <v-list-item-title class="text-h6"> Navi </v-list-item-title>
           <v-list-item-subtitle> hoge hoge </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -13,23 +11,23 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item>
+        <v-list-item v-on:click="changePage('/search')">
           <v-list-item-icon>
             <v-icon>mdi-view-dashboard</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>
-              <router-link to="/search">Search</router-link>
+              Search
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item>
+        <v-list-item  v-on:click="changePage('/list')">
           <v-list-item-icon>
             <v-icon>mdi-view-dashboard</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>
-              <router-link to="/list">List</router-link>
+              List
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
@@ -37,7 +35,7 @@
     </v-navigation-drawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-       Saitama Premium
+      Saitama Premium
     </v-app-bar>
     <v-main>
       <v-container fluid>
@@ -55,6 +53,12 @@ export default {
     return {
       drawer: false,
     };
+  },
+  methods: {
+    changePage: function (page) {
+      this.$router.push(page);
+      this.drawer = false;
+    },
   },
 };
 </script>
