ada784aaa17d037d2791fc08377963d606230130
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-12 21:48:10 +0900

    add merchant info

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -38,13 +38,21 @@
           map-type-id="terrain"
           style="width: 500px; height: 300px"
         >
+          <GmapInfoWindow
+            :options="infoOptions"
+            :position="infoWindowPos"
+            :opened="infoWinOpen"
+            @closeclick="infoWinOpen = false"
+          >
+            {{infoDisplay}}
+          </GmapInfoWindow>
           <GmapMarker
             :key="index"
-            v-for="(merchant, index) in filteredMerchants"
-            :position="merchant.position"
+            v-for="(m, index) in filteredMerchants"
+            :position="m.position"
             :clickable="true"
-            :draggable="true"
-            @click="center=m.position"
+            :draggable="false"
+            @click="toggleInfoWindow(m)"
           />
         </GmapMap>
       </v-col>
@@ -66,10 +74,10 @@
 </template>
 
 <script>
-import Merchant from "../../data/merchant.json"
-import Area from "../../data/area.json"
-import Category from "../../data/category.json"
-import TicketType from "../../data/ticket_type.json"
+import Merchant from "../../data/merchant.json";
+import Area from "../../data/area.json";
+import Category from "../../data/category.json";
+import TicketType from "../../data/ticket_type.json";
 
 export default {
   name: "SearchPage",
@@ -91,102 +99,87 @@ export default {
         { text: "Address", value: "address" },
         { text: "Ticket", value: "ticket" },
       ],
-    }
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35,
+        },
+      },
+      infoWindowPos: null,
+      infoWinOpen: true,
+      infoDisplay: "",
+    };
   },
   computed: {
     filteredMerchants: function () {
-
-      var _mi = this.merchants
-      var _mo = []
-      var i = 0
-      var j = 0
+      var _mi = this.merchants;
+      var _mo = [];
+      var i = 0;
+      var j = 0;
 
       // search area
       if (this.selected_area != null && this.selected_area != "ALL") {
         for (i in _mi) {
           if (_mi[i].address.indexOf(this.selected_area) > -1) {
-            _mo.push(_mi[i])
+            _mo.push(_mi[i]);
           }
         }
-        _mi = _mo
+        _mi = _mo;
       }
       // search category
       if (this.selected_category != null && this.selected_category != "ALL") {
-        _mo = []
-        i = 0
+        _mo = [];
+        i = 0;
         for (i in _mi) {
           if (_mi[i].category == this.selected_category) {
-            _mo.push(_mi[i])
+            _mo.push(_mi[i]);
           }
         }
-        _mi = _mo
+        _mi = _mo;
       }
       // search paper_electronic
       if (
         this.selected_paper_electronic != null &&
         this.selected_paper_electronic != "ALL"
       ) {
-        _mo = []
-        i = 0
-        j = 0
+        _mo = [];
+        i = 0;
+        j = 0;
         for (i in _mi) {
           for (j in _mi[i].ticket) {
             if (_mi[i].ticket[j] == this.selected_paper_electronic) {
-              _mo.push(_mi[i])
+              _mo.push(_mi[i]);
             }
           }
         }
-        _mi = _mo
+        _mi = _mo;
       }
       // search private_common
       if (
         this.selected_private_common != null &&
         this.selected_private_common != "ALL"
       ) {
-        _mo = []
-        i = 0
-        j = 0
+        _mo = [];
+        i = 0;
+        j = 0;
         for (i in _mi) {
           for (j in _mi[i].ticket) {
             if (_mi[i].ticket[j] == this.selected_private_common) {
-              _mo.push(_mi[i])
+              _mo.push(_mi[i]);
             }
           }
         }
       }
-      _mo.position
-      return _mo
+      _mo.position;
+      return _mo;
     },
   },
-  mounted() {
-    let timer = setInterval(() => {
-      if (window.google) {
-        clearInterval(timer)
-        const map = new window.google.maps.Map(this.$refs.map, {
-          center: new window.google.maps.LatLng(35.890859, 139.615088),
-          zoom: 12,
-        })
-        var k = 0
-        for (k in this.merchants) {
-          // maker
-          const maker = new window.google.maps.Marker({
-            map: map,
-            position: new window.google.maps.LatLng(
-              Number(this.merchants[k].lat),
-              Number(this.merchants[k].lng)
-            ),
-          })
-          // info window
-          const info = new window.google.maps.InfoWindow({
-            content: this.merchants[k].name,
-          })
-          // event
-          maker.addListener("click", function () {
-            info.open(map, maker)
-          })
-        }
-      }
-    }, 3000)
+  methods: {
+    toggleInfoWindow(marker) {
+      this.infoWindowPos = marker.position;
+      this.infoWinOpen = true;
+      this.infoDisplay = marker.name;
+    },
   },
-}
+};
 </script>
