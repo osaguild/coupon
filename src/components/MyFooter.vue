bb3c0bb211707dce09fda7cc0fbcb52ff535f907
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-15 22:31:51 +0900

    split app.vue to componets

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="12">
+      <strong>@ {{ new Date().getFullYear() }} osaguild.com</strong>
+    </v-col>
+  </v-footer>
+</template>
+<script>
+export default {
+  name: "MyFooter",
+};
+</script>
