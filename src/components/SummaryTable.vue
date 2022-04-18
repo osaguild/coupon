481c2636e0f3d5c1862537825dac8b3e6922dfc0
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-18 13:22:12 +0900

    fix table width

--- src/components/SummaryTable.vue
+++ src/components/SummaryTable.vue
@@ -1,50 +1,59 @@
 <template>
-    <v-expansion-panels>
-      <v-expansion-panel>
-        <v-expansion-panel-header> Paper Electronic </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-data-table
-            :headers="headers"
-            :items="summary.paper_electronic"
-            hide-default-footer
-            class="elevation-1"
-          ></v-data-table>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-      <v-expansion-panel>
-        <v-expansion-panel-header> Private Common </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-data-table
-            :headers="headers"
-            :items="summary.private_common"
-            hide-default-footer
-            class="elevation-1"
-          ></v-data-table>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-      <v-expansion-panel>
-        <v-expansion-panel-header> Area </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-data-table
-            :headers="headers"
-            :items="summary.area"
-            hide-default-footer
-            class="elevation-1"
-          ></v-data-table>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-      <v-expansion-panel>
-        <v-expansion-panel-header> Category </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-data-table
-            :headers="headers"
-            :items="summary.category"
-            hide-default-footer
-            class="elevation-1"
-          ></v-data-table>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-header> Paper Electronic </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-simple-table> </v-simple-table>
+        <v-data-table
+          :headers="headers"
+          :items="summary.paper_electronic"
+          :items-per-page="-1"
+          class="elevation-1"
+          disable-sort
+          hide-default-footer
+        ></v-data-table>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header> Private Common </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-data-table
+          :headers="headers"
+          :items="summary.private_common"
+          :items-per-page="-1"
+          class="elevation-1"
+          disable-sort
+          hide-default-footer
+        ></v-data-table>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header> Area </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-data-table
+          :headers="headers"
+          :items="summary.area"
+          :items-per-page="-1"
+          class="elevation-1"
+          disable-sort
+          hide-default-footer
+        ></v-data-table>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header> Category </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-data-table
+          :headers="headers"
+          :items="summary.category"
+          :items-per-page="-1"
+          class="elevation-1"
+          disable-sort
+          hide-default-footer
+        ></v-data-table>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
@@ -55,8 +64,14 @@ export default {
   data() {
     return {
       headers: [
-        { text: "Name", align: "start", sortable: false, value: "name" },
-        { text: "Count", value: "count" },
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+          width: "50%",
+        },
+        { text: "Count", value: "count", width: "50%" },
       ],
       summary: Summary,
     };
