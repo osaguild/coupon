dd3375ccab12f17a339029def5ca0f61f31160d6
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-01 08:24:48 +0900

    success deploy

--- lib/saitama-premium-stack.ts
+++ lib/saitama-premium-stack.ts
@@ -1,37 +1,35 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as r53 from 'aws-cdk-lib/aws-route53';
-import * as r53Targets from 'aws-cdk-lib/aws-route53-targets';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps, Construct, RemovalPolicy } from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deploy from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as r53 from '@aws-cdk/aws-route53';
+import * as r53Targets from '@aws-cdk/aws-route53-targets';
+import * as iam from '@aws-cdk/aws-iam';
 
 
 export class SaitamaPremiumStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const TARGET = this.node.tryGetContext('dev.TARGET');
-    const DOMAIN = this.node.tryGetContext('dev.DOMAIN');
-    const FRONT_DOMAIN = this.node.tryGetContext('dev.FRONT_DOMAIN');
-    const CERTIFICATE_DOMAIN = this.node.tryGetContext('dev.CERTIFICATE_DOMAIN');
+    const TARGET = "dev";
+    const DOMAIN = "dev.osaguild.com";
+    const FRONT_DOMAIN = "saitama-premium.dev.osaguild.com";
+    const CERTIFICATE_DOMAIN = "*.dev.osaguild.com";
 
     // s3 bucket
     const bucket = new s3.Bucket(
       this, "saitama-premium-bucket", {
       websiteIndexDocument: "index.html",
       accessControl: s3.BucketAccessControl.PRIVATE,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      bucketName: `saitama-premium-bucket-${TARGET}`
+      removalPolicy: RemovalPolicy.DESTROY,
+      bucketName: `saitama-premium-bucket-${TARGET}`,
     });
 
     // s3 access identity
     const identity = new cloudfront.OriginAccessIdentity(
       this, "saitama-premium-origin-access-identity", {
-      comment: `s3 access identity ${TARGET}`
+      comment: `s3 access identity ${TARGET}`,
     });
 
     // s3 policy statement
@@ -52,7 +50,7 @@ export class SaitamaPremiumStack extends Stack {
       // deploy dir is ./dist
       sources: [s3Deploy.Source.asset("./dist")],
       // todo: meaning of retain_on_delete
-      retainOnDelete: false
+      retainOnDelete: false,
     });
 
     // hosted zone
