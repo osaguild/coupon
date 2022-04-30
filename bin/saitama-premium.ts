6a41f630a6926f73843d05f728585009da2d36cd
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-30 11:48:01 +0900

    fix

--- bin/saitama-premium.ts
+++ bin/saitama-premium.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { SaitamaPremiumStack } from '../lib/saitama-premium-stack';
 
 const app = new cdk.App();
-new SaitamaPremiumStack(app, 'SaitamaPremiumStack', {
+new SaitamaPremiumStack(app, 'saitama-premium-test', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/saitama-premium-stack.ts
+++ lib/saitama-premium-stack.ts
@@ -14,19 +14,24 @@ export class SaitamaPremiumStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const TARGET = this.node.tryGetContext('dev.TARGET');
+    const DOMAIN = this.node.tryGetContext('dev.DOMAIN');
+    const FRONT_DOMAIN = this.node.tryGetContext('dev.FRONT_DOMAIN');
+    const CERTIFICATE_DOMAIN = this.node.tryGetContext('dev.CERTIFICATE_DOMAIN');
+
     // s3 bucket
     const bucket = new s3.Bucket(
       this, "saitama-premium-bucket", {
       websiteIndexDocument: "index.html",
       accessControl: s3.BucketAccessControl.PRIVATE,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      bucketName: "saitama-premium-bucket"
+      bucketName: `saitama-premium-bucket-${TARGET}`
     });
 
     // s3 access identity
     const identity = new cloudfront.OriginAccessIdentity(
       this, "saitama-premium-origin-access-identity", {
-      comment: "s3 access identity"
+      comment: `s3 access identity ${TARGET}`
     });
 
     // s3 policy statement
@@ -48,6 +53,56 @@ export class SaitamaPremiumStack extends Stack {
       sources: [s3Deploy.Source.asset("./dist")],
       // todo: meaning of retain_on_delete
       retainOnDelete: false
+    });
+
+    // hosted zone
+    const hostedZone = r53.HostedZone.fromLookup(
+      this, "hosted-zone", {
+      domainName: "osaguild.com",
+    });
+
+    // certificate
+    const certificate = new acm.DnsValidatedCertificate(
+      this, "certificate", {
+      domainName: CERTIFICATE_DOMAIN,
+      subjectAlternativeNames: [CERTIFICATE_DOMAIN],
+      hostedZone: hostedZone,
+      region: "us-east-1",
+    });
+
+    // cloud front
+    const front = new cloudfront.CloudFrontWebDistribution(
+      this, "saitama-premium-front", {
+      defaultRootObject: "index.html",
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      httpVersion: cloudfront.HttpVersion.HTTP2,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: bucket,
+          originAccessIdentity: identity
+        },
+        behaviors: [{
+          isDefaultBehavior: true,
+        }],
+      }],
+      viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(
+        certificate, {
+        aliases: [FRONT_DOMAIN],
+        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2019,
+        sslMethod: cloudfront.SSLMethod.SNI
+      }),
+      comment: `saitama-premium-front-${TARGET}`,
+    });
+
+    // A record for front
+    const frontARecord = new r53.ARecord(
+      this, "front-a-record", {
+      recordName: FRONT_DOMAIN,
+      zone: hostedZone,
+      target: r53.RecordTarget.fromAlias(
+        new r53Targets.CloudFrontTarget(front)
+      ),
     });
   }
 }
