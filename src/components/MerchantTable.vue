ab85ceffc0f40bfd185372d01fb58fdfd4af7fe2
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-02 14:27:38 +0900

    update footer

--- src/components/MerchantTable.vue
+++ src/components/MerchantTable.vue
@@ -1,22 +1,34 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="merchants"
-    :items-per-page="10"
-    class="elevation-1"
-    disable-sort
-  >
-    <template v-slot:[`item.category`]="{ item }">
-      <v-chip>
-        {{ item.category }}
-      </v-chip>
-    </template>
-    <template v-slot:[`item.ticket`]="{ item }">
-      <v-chip v-for="ticket in item.ticket" :key="ticket">
-        {{ ticket }}
-      </v-chip>
-    </template>
-  </v-data-table>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="merchants"
+      :items-per-page="10"
+      :page.sync="page"
+      class="elevation-1"
+      disable-sort
+      hide-default-footer
+      @page-count="pageCount = $event"
+    >
+      <template v-slot:[`item.category`]="{ item }">
+        <v-chip>
+          {{ item.category }}
+        </v-chip>
+      </template>
+      <template v-slot:[`item.ticket`]="{ item }">
+        <v-chip v-for="ticket in item.ticket" :key="ticket">
+          {{ ticket }}
+        </v-chip>
+      </template>
+    </v-data-table>
+    <div class="text-center pt-2">
+      <v-pagination 
+      v-model="page"
+      :length="pageCount"
+      :total-visible="7"
+      ></v-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,6 +42,8 @@ export default {
         { text: "Category", value: "category" },
         { text: "Ticket", value: "ticket" },
       ],
+      page: 1,
+      pageCount: 0,
     };
   },
   computed: {
