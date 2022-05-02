20576ebe1642aafc44e05c6d2db79b67566433b1
nobuhisa.abe <33506754+osaguild@users.noreply.github.com>
2022-05-02 16:39:11 +0900

    feature/#54

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <search-form :param="form" @update:form="form = $event"></search-form>
-    <my-map :markers="getMerchants"></my-map>
+    <my-map :markers="getMerchants" :center="getCenter"></my-map>
     <merchant-table
       :param="getMerchants"
     ></merchant-table>
@@ -25,11 +25,26 @@ export default {
         paper_electronic: null,
         private_common: null,
       },
+      latLans: {
+        "西区": { lat: 35.9129, lng: 139.5789 },
+        "北区": { lat: 35.9315, lng: 139.6203 },
+        "大宮区": { lat: 35.9064, lng: 139.6287 },
+        "見沼区": { lat: 35.9352, lng: 139.6545 },
+        "中央区": { lat: 35.884, lng: 139.6261 },
+        "桜区": { lat: 35.8637, lng: 139.6043 },
+        "浦和区": { lat: 35.8616, lng: 139.6454 },
+        "南区": { lat: 35.8427, lng: 139.6655 },
+        "緑区": { lat: 35.871, lng: 139.6839 },
+        "岩槻区": { lat: 35.9498, lng: 139.6941 },
+      },
     };
   },
   computed: {
     getMerchants: function () {
       return Merchant.search(this.form);
+    },
+    getCenter: function () {
+      return this.latLans[this.form.area];
     },
   },
 };
